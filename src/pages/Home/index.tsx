e31f8622cc2a51b5fa112a77e856d17af60240be
zhang <602079316@qq.com>
2024-11-13 14:29:32 +0800

    提交

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,12 +4,12 @@ import { Button, Input } from 'antd'
 import { useState, useEffect } from 'react'
 
 const Home = () => {
-    const socket = io('http://localhost:3001')
-    useEffect(() => {
-        socket.on('message', (data) => {
-            console.log(data)
-        })
-    }, [])
+    // const socket = io('http://localhost:3001')
+    // useEffect(() => {
+    //     socket.on('message', (data) => {
+    //         console.log(data)
+    //     })
+    // }, [])
 
     const [message, setMessage] = useState('')
 
@@ -18,7 +18,7 @@ const Home = () => {
             <div className={homeStyle['home-left']}>
                 <Input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="请输入" style={{ width: 300, margin: '10px 0' }} />
                 <br />
-                <Button type="primary" onClick={() => { socket.emit('message', { message }) }}>按钮</Button>
+                {/* <Button type="primary" onClick={() => { socket.emit('message', { message }) }}>按钮</Button> */}
             </div>
             <div>
             </div>
